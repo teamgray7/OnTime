500316e5d33f19dbd3eab8c1300578c6c7ff5233
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-02-20 17:06:57 +0200

    Changed the fragment placed on main activity to see the result.

--- app/src/main/java/com/example/ontime/MainActivity.java
+++ app/src/main/java/com/example/ontime/MainActivity.java
@@ -118,9 +118,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            View rootView = inflater.inflate(R.layout.fragment_statistics, container, false);
+            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
