0e1387506b8568ef19e85d7eb40f8098321d6d12
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-03-04 12:21:27 +0200

    Login Fragment now does not take any arguments.

--- app/src/main/java/com/unioulu/ontime/fragment/LoginFragment.java
+++ app/src/main/java/com/unioulu/ontime/fragment/LoginFragment.java
@@ -11,21 +11,12 @@ import com.unioulu.ontime.R;
 
 public class LoginFragment extends Fragment {
 
-    private static final String ARG_SECTION_NUMBER = "section_number";
-
     public LoginFragment() {
         // Empty constructor
     }
 
-    /**
-     * Returns a new instance of this fragment for the given section
-     * number.
-     */
-    public static LoginFragment newInstance(int sectionNumber) {
+    public static LoginFragment newInstance() {
         LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-        fragment.setArguments(args);
         return fragment;
     }
 
