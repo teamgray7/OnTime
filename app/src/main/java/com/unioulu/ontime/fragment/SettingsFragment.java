4b6462b72ed2a96618f57f8d07dd923fbb7025ff
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-03-04 18:01:34 +0200

    Settings fragment is created.

---
+++ app/src/main/java/com/unioulu/ontime/fragment/SettingsFragment.java
@@ -0,0 +1,37 @@
+package com.unioulu.ontime.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.unioulu.ontime.R;
+
+public class SettingsFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public SettingsFragment() {
+        // Empty constructor
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static SettingsFragment newInstance(int sectionNumber) {
+        SettingsFragment fragment = new SettingsFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        return rootView;
+    }
+}
