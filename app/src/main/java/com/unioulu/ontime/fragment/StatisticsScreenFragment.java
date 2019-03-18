3c29dd0cb7e9f6039be380791914cea3ece27823
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-03-18 16:51:07 +0200

    Found a nice statistic library and wrote an example.

--- app/src/main/java/com/unioulu/ontime/fragment/StatisticsScreenFragment.java
+++ app/src/main/java/com/unioulu/ontime/fragment/StatisticsScreenFragment.java
@@ -7,11 +7,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.unioulu.ontime.R;
+
+import java.util.ArrayList;
 
 public class StatisticsScreenFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private PieChart pieChart;
 
     public StatisticsScreenFragment() {
         // Empty constructor
@@ -32,6 +40,28 @@ public class StatisticsScreenFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_statistics, container, false);
+        setPieChart(rootView);
+
         return rootView;
+    }
+
+    /*
+        This functions set statistical data and assign them into a pie chart.
+        TODO : Retrieve data from database and place them into pieEntries arrayList.
+     */
+    private void setPieChart(View rootView) {
+        ArrayList<PieEntry> pieEntries = new ArrayList<PieEntry>();
+        pieEntries.add(new PieEntry(81f, "Success"));
+        pieEntries.add(new PieEntry(18f, "Danger"));
+        pieEntries.add(new PieEntry(1f, "Fail"));
+
+        PieDataSet dataSet = new PieDataSet(pieEntries, "Statistics of medicine");
+        PieData data = new PieData(dataSet);
+
+        pieChart = (PieChart) rootView.findViewById(R.id.pie_chart);
+        pieChart.setData(data);
+
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieChart.animateXY(2000,2000);
     }
 }
