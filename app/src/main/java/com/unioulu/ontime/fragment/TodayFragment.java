af3c413258e472b282e632aa4bff1e8ea8f5e982
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-04-13 11:04:55 +0300

    We have now one list that shows all the pills. (Still no colors on status)

--- app/src/main/java/com/unioulu/ontime/fragment/TodayFragment.java
+++ app/src/main/java/com/unioulu/ontime/fragment/TodayFragment.java
@@ -1,7 +1,5 @@
 package com.unioulu.ontime.fragment;
 
-import android.content.Intent;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -10,9 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.unioulu.ontime.AlarmActivity;
 import com.unioulu.ontime.R;
 import com.unioulu.ontime.RecyclerViewAdapter;
 
@@ -24,11 +20,6 @@ public class TodayFragment extends Fragment {
 
     private ArrayList<String> mNamesNext = new ArrayList<>();
     private ArrayList<String> mImageUrlsNext= new ArrayList<>();
-    private ArrayList<String> mNamesPrevious = new ArrayList<>();
-    private ArrayList<String> mImageUrlsPrevious= new ArrayList<>();
-
-    private TextView nextPills;
-    private TextView previousPills;
 
     public TodayFragment() {
         // Empty constructor
@@ -50,34 +41,12 @@ public class TodayFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_today, container, false);
 
-        RecyclerView nextPillsRV = rootView.findViewById(R.id.NextPillRecyclerView);
-        RecyclerView alreadyTakenPillsRV = rootView.findViewById(R.id.AlreadyTakenRecyclerView);
-
         initImageBitmaps();
 
+        RecyclerView nextPillsRV = rootView.findViewById(R.id.rv_pillList);
         RecyclerViewAdapter nextPillsAdapter = new RecyclerViewAdapter(getContext(), mNamesNext, mImageUrlsNext);
-        RecyclerViewAdapter alreadyTakenPillsAdapter = new RecyclerViewAdapter(getContext(), mNamesPrevious, mImageUrlsPrevious);
-
         nextPillsRV.setAdapter(nextPillsAdapter);
-        alreadyTakenPillsRV.setAdapter(alreadyTakenPillsAdapter);
-
         nextPillsRV.setLayoutManager(new LinearLayoutManager(getContext()));
-        alreadyTakenPillsRV.setLayoutManager(new LinearLayoutManager(getContext()));
-
-        nextPills = (TextView) rootView.findViewById(R.id.nextPillsTextView);
-        previousPills = (TextView) rootView.findViewById(R.id.alreadyTakenPillsTextView);
-        nextPills.setPaintFlags(nextPills.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-        previousPills.setPaintFlags(previousPills.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-
-        // Should be removed ! Only implemented to make alarm activity accessible !
-        nextPills.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Intent intent = new Intent(v.getContext(), AlarmActivity.class);
-                startActivity(intent);
-                return true;
-            }
-        });
 
         return rootView;
     }
@@ -86,8 +55,6 @@ public class TodayFragment extends Fragment {
         // Clearing so that there won't be duplicate items.
         mImageUrlsNext.clear();
         mNamesNext.clear();
-        mImageUrlsPrevious.clear();
-        mNamesPrevious.clear();
 
         mImageUrlsNext.add("https://i.ibb.co/f00N4B3/20190303-224419.jpg");
         mNamesNext.add("Sinecod 50mg - 1 pill");
@@ -97,11 +64,5 @@ public class TodayFragment extends Fragment {
 
         mImageUrlsNext.add("https://i.ibb.co/LCMXjwk/20190303-224431.jpg");
         mNamesNext.add("Katarin - 0.5 pill");
-
-        mImageUrlsPrevious.add("https://i.ibb.co/LCMXjwk/20190303-224431.jpg");
-        mNamesPrevious.add("Katarin - 0.5 pill - 08:00");
-
-        mImageUrlsPrevious.add("https://i.ibb.co/6ZZj4cJ/20190303-224445.jpg");
-        mNamesPrevious.add("Croxilex - 1 pill - 08:00");
     }
 }
