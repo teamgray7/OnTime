136e037c789f39256c570781a7ca29127ad7806e
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-04-22 16:55:00 +0300

    Register fragment is revisited. All bugs are solved.

--- app/src/main/java/com/unioulu/ontime/LoginRegisterActivity.java
+++ app/src/main/java/com/unioulu/ontime/LoginRegisterActivity.java
@@ -12,8 +12,10 @@ import com.unioulu.ontime.fragment.LoginFragment;
 import com.unioulu.ontime.fragment.RegisterFragment;
 
 public class LoginRegisterActivity extends AppCompatActivity
-    implements LoginFragment.OnFragmentInteractionListener {
+    implements LoginFragment.OnFragmentInteractionListener,
+    RegisterFragment.OnFragmentInteractionListener {
 
+    private final String TAG_LOGIN_FRAGMENT = "fragment_login";
     private final String TAG_REGISTER_FRAGMENT = "fragment_register";
     private final String TAG_FORGET_PASSWORD_FRAGMENT = "fragment_forget_password";
 
@@ -61,5 +63,17 @@ public class LoginRegisterActivity extends AppCompatActivity
         adminUser.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(adminUser);
         finish();
+    }
+
+    @Override
+    public void accountCreated() {
+        LoginFragment loginFragment = LoginFragment.newInstance();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim
+                .enter_from_left, R.anim.exit_to_right);
+        transaction.replace(R.id.login_content, loginFragment, TAG_LOGIN_FRAGMENT);
+        transaction.addToBackStack(TAG_LOGIN_FRAGMENT);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/unioulu/ontime/fragment/RegisterFragment.java
+++ app/src/main/java/com/unioulu/ontime/fragment/RegisterFragment.java
@@ -1,9 +1,9 @@
 package com.unioulu.ontime.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,78 +14,128 @@ import android.widget.Toast;
 import com.unioulu.ontime.R;
 import com.unioulu.ontime.database_classes.DataHolder;
 import com.unioulu.ontime.database_classes.UsersTable;
+import com.unioulu.ontime.helper.StringFormatChecker;
 
 public class RegisterFragment extends Fragment {
 
-
-
     private EditText inputUsername;
     private EditText inputPassword;
+    private EditText inputPassword2;
     private EditText inputEmail;
     private Button signupBtn;
+
+    // The interaction listener is defined.
+    private OnFragmentInteractionListener mListener;
 
     public RegisterFragment() {
         // Empty constructor
     }
 
     public static RegisterFragment newInstance() {
-        RegisterFragment fragment = new RegisterFragment();
-        return fragment;
+        return new RegisterFragment();
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_create_account, container, false);
 
-
-
         inputUsername = rootView.findViewById(R.id.inputUsername);
         inputPassword = rootView.findViewById(R.id.inputPassword);
+        inputPassword2 = rootView.findViewById(R.id.inputRetypePassword);
         inputEmail = rootView.findViewById(R.id.inputEmail);
+
         signupBtn = rootView.findViewById(R.id.signUpButton);
-
         signupBtn.setOnClickListener(signUpClicked);
-
 
         return rootView;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
 
     private View.OnClickListener signUpClicked = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            // final String username = etUsername.getText().toString();
-            // final String password = etPassword.getText().toString();
-            final  String username = inputUsername.getText().toString();
-            final  String password = inputPassword.getText().toString();
-            final  String email = inputEmail.getText().toString();
+            final String username = inputUsername.getText().toString();
+            final String password = inputPassword.getText().toString();
+            final String password2 = inputPassword2.getText().toString();
+            final String email = inputEmail.getText().toString();
 
-            // TODO: implement a mechanist to save user to database
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
+            if(username.equals("") || password.equals("") || password2.equals("") || email.equals("")) {
+                String message = "Please fill all the fields..";
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+            } else if(!password.equals(password2)) {
+                String message = "Passwords do not match.";
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+            } else if(password.length() < 7) {
+                String message = "Password should contain at least 7 characters.";
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+            } else if(!StringFormatChecker.isEmailValid(email)) {
+                String message = "Email address does not appear to be valid.";
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+            } else {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        UsersTable user = new UsersTable(
+                                username,
+                                email,
+                                password,
+                                true
+                        );
 
-                    UsersTable user = new UsersTable(
-                            username,
-                            email,
-                            password,
-                            true
-                    );
-                    try {
-                        DataHolder.getInstance().getAppDatabase().usersTableInterface().createUser(user); Log.d("Logging fragment", "User added !");
-                    }catch (Exception e){
+                        try {
+                            DataHolder.getInstance().getAppDatabase().usersTableInterface().createUser(user);
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    String message = "User " + username + " is created!" ;
+                                    Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show();
 
-                        getActivity().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getActivity().getApplicationContext(), "Email already exists !", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                        Log.d("Logging fragment", "Email already exists !");
+                                    mListener.accountCreated();
+                                }
+                            });
+                        } catch (Exception e){
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    String message = "Username or email already exists!";
+                                    Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
                     }
-
-                }
-            }).start();
+                }).start();
+            }
         }
     };
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void accountCreated();
+    }
 }
